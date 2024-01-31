b6886a0664442c6b72c8825b6001564dde1b4052
Andrew Collins <collina2@uci.edu>
2024-01-30 19:48:51 -0800

    Initial Commit

---
+++ HealthHomies/HealthHomiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthHomiesApp.swift
+//  HealthHomies
+//
+//  Created by Andrew Collins on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthHomiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
