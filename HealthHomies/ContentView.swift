f191e57ea5cd60c8f7ad6ade5d05e756ad201040
Rithvij <rithvij315@gmail.com>
2024-03-14 10:29:28 -0700

    can query data now

--- HealthHomies/ContentView.swift
+++ HealthHomies/ContentView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 import FirebaseAnalyticsSwift
 import FirebaseAnalytics
 import HealthKit
+import FirebaseCore
+import FirebaseFirestore
+
+
 
 struct ContentView: View {
     @EnvironmentObject var manager: HealthManager
+    
+    // This is imported so the funcitions inside the class can be used
+    @StateObject var dbManager = FirestoreManager()
+    
     
     var body: some View {
         // TODO: have the user enter personal data here
@@ -25,6 +33,25 @@ struct ContentView: View {
             ForEach(manager.userData.sorted(by: { $0.key < $1.key }), id: \.key) { item in
                 Text("\(item.key): \(item.value)")
             }
+            Text("")
+            
+            
+            Text("test query data")
+            Button("Fetch Data") {
+                //The next few lines just waits fort the function to be called, and then the data is printed to the console
+                            Task {
+                                await dbManager.fetchFirebaseData()
+                            }
+            }
+            // Here it checks the fetchedData array from the TestQuery file and prints that out if it's not empty
+            if !dbManager.fetchedData.isEmpty {
+                            List(dbManager.fetchedData, id: \.self) { data in
+                                Text(data) // Modify this according to your data structure
+                            }
+                        } else {
+                            Text("No data fetched yet")
+                                .foregroundColor(.gray)
+                        }
         }
         .padding()
         .analyticsScreen(name: "\(ContentView.self)", extraParameters: ["test2": "test2 value"])
---
+++ HealthHomies/TestQuery.swift
@@ -0,0 +1,40 @@
+//
+//  TestQuery.swift
+//  HealthHomies
+//
+//  Created by Rithvij Pochampally on 3/14/24.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseAnalyticsSwift
+import FirebaseAnalytics
+import FirebaseCore
+import FirebaseFirestore
+
+let db = Firestore.firestore()
+let exerciseRef = db.collection("Exercise")
+
+class FirestoreManager: ObservableObject {
+    @Published var fetchedData: [String] = []
+    
+    func fetchFirebaseData() async {
+        
+        //This query is what is used to get data
+        //This link helps show how to query different things:
+        // https://firebase.google.com/docs/firestore/query-data/queries
+        // Ask Rithvij for any questions on how to query or for any help!
+        let query = exerciseRef.whereField("muscle_groups", arrayContains: "Triceps")
+        do {
+            let querySnapshot = try await query.getDocuments()
+            
+            // This next line is just to add it to an array that can be read from other views as long as the FirestoreManager class is imported correctly
+            fetchedData = querySnapshot.documents.compactMap { $0.data()["name"] as? String} // Like a dictionary, just use the names in the firestore database (ex: name, id, muscle_groups, etc)
+            for document in querySnapshot.documents {
+            print("\(document.documentID) => \(document.data())")
+          }
+        }   catch {
+            print("Error getting documents: \(error)")
+        }
+    }
+}
